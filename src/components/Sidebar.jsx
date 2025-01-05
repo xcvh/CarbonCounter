41ee659208fff64121538a6c62cbec557cca4482
xcvh <hi@jel.do>
2025-01-05 20:28:02 +0100

    feat: add Sidebar component to main application layout
    Introduce a new Sidebar component that contains a list of links. 
    Integrate the Sidebar into the main App component to enhance 
    navigation and improve user experience. This change aims to 
    provide users with quick access to various sections of the 
    application.

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+function Sidebar() {
+  return (
+    <div>
+      <a href="">A</a>
+      <br />
+      <a href="">bunch</a>
+      <br />
+      <a href="">of</a>
+      <br />
+      <a href="">links</a>
+      <br />
+    </div>
+  );
+}
+
+export default Sidebar;
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,11 +1,13 @@
 import LandingPage from "./LandingPage";
 import ResultsPage from "./ResultsPage";
 import CalculatorPage from "./CalculatorPage";
+import Sidebar from "../components/Sidebar";
 
 function App() {
   return (
     <div>
       CarbonCounter
+      <Sidebar />
       <LandingPage />
       <ResultsPage />
       <CalculatorPage />
