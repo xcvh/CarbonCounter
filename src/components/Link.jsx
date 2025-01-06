4b89e2e31215f23b7aea544143c9543ec068e99d
xcvh <hi@jel.do>
2025-01-06 09:41:36 +0100

    feat: add Link component for navigation handling
    Introduce a new Link component utilizes the NavigationContextto handle navigation within application. The component prevents 
    default anchor behavior and triggers the navigate function with the 
    provided 'to' prop, enhancing user experience by enabling seamless 
    in-app navigation.

---
+++ src/components/Link.jsx
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import NavigationContext from "../context/navigation";
+
+function Link({ to, children }) {
+  const { navigate } = useContext(NavigationContext);
+
+  const handleClick = (event) => {
+    event.preventDefault();
+
+    navigate(to);
+  };
+
+  return <a onClick={handleClick}>{children}</a>;
+}
