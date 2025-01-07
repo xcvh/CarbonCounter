c0900c7bbac6fdbfc2086b3d0a13bd22c0d8b4d6
xcvh <hi@jel.do>
2025-01-07 07:44:39 +0100

    babysteps: begin to styel Sidebar component with icons and full layout
    Add icons to sidebar links for improved navigation clarity. 
    Refactor the sidebar layout to use a more structured 
    HTML5 semantic approach with `<>` and `<nav>` elements. 
    Adjust the main application layout to accommodate the new 
    sidebar design, ensuring a responsive and user-friendly 
    interface.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,29 +1,78 @@
+import {
+  Calculator,
+  ChevronFirst,
+  Home,
+  LayoutDashboard,
+  MoreVertical,
+} from "lucide-react";
 import Link from "./Link";
 
 function Sidebar() {
   const links = [
-    { label: "Home", path: "/" },
-    { label: "Calculator", path: "/calculator" },
-    { label: "Results", path: "/results" },
+    { label: "Home", path: "/", icon: <Home size={20} /> },
+    {
+      label: "Calculator",
+      path: "/calculator",
+      icon: <Calculator size={20} />,
+    },
+    { label: "Results", path: "/results", icon: <LayoutDashboard size={20} /> },
   ];
 
   const renderedLinks = links.map((link) => {
     return (
-      <Link
+      <li
         key={link.label}
-        to={link.path}
-        className="mb-1 text-white hover:underline"
-        activeClassName="font-bold border-l-4 pl-2 border-white"
+        className="relative flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer"
       >
-        {link.label}
-      </Link>
+        {link.icon}
+        <Link
+          to={link.path}
+          className="text-green-900"
+          activeClassName="font-bold"
+        >
+          {link.label}
+        </Link>
+      </li>
     );
   });
 
   return (
-    <div className="sticky top-0 overscroll-y-scroll flex flex-col items-start bg-green-800">
-      {renderedLinks}
-    </div>
+    <aside className="h-screen col-span-2">
+      <nav className="h-full flex flex-col bg-green-50 border-r shadow-sm">
+        <div className="p-4 pb-2 flex justify-between items-center">
+          <img
+            src="https://img.logoipsum.com/254.svg"
+            className="w-32"
+            alt="Logo placeholder"
+          />
+          <button className="p-1.5 rounded-lg bg-green-100 hover:bg-green-200">
+            <ChevronFirst />
+          </button>
+        </div>
+
+        <ul className="flex-1 px-3">{renderedLinks}</ul>
+
+        <div className="border-t flex p-3">
+          <img
+            src="https://ui-avatars.com/api/?name=Jeldo+Meppen"
+            alt="Placeholder Avatar"
+            className="w-10 h-10 rounded-md"
+          />
+          <div
+            className={`
+            flex justify-between items-center
+            w-52 ml-3
+            `}
+          >
+            <div className="leading-4">
+              <h4 className="font-semibold">Jeldo Meppen</h4>
+              <span className="text-xs text-green-600">hi@jel.do</span>
+            </div>
+            <MoreVertical size={20} />
+          </div>
+        </div>
+      </nav>
+    </aside>
   );
 }
 
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -6,9 +6,9 @@ import Route from "../components/Route";
 
 function App() {
   return (
-    <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+    <div className="container grid grid-cols-6 gap-4">
       <Sidebar />
-      <div className="col-span-5">
+      <div className="col-span-4">
         <h1 className="text-xl font-serif font-bold text-green-700">
           CarbonCounter
         </h1>
