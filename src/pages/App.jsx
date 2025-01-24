013c3008cf20d4938925aecfb0c122ca8ac1059c
xcvh <hi@jel.do>
2025-01-24 16:59:28 +0100

    refactor: clean up CalculatorPage and update imports
    Refactor the CalculatorPage component to code 
    readability by adjusting indentation and adding a container 
    class for better styling. Update the import statement in 
    App.jsx to reflect the correct path for the Components 
    module. Remove the unused LandingPage component to 
    streamline the codebase and enhance maintainability.

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,4 +1,4 @@
-import Components from "./LandingPage";
+import Components from "./Components";
 import ResultsPage from "./ResultsPage";
 import CalculatorPage from "./CalculatorPage";
 import Sidebar from "../components/Sidebar";
@@ -6,26 +6,26 @@ import Route from "../components/Route";
 import { SidebarProvider } from "../context/sidebar";
 
 function App() {
-  return (
-    <main className="relative flex min-h-screen bg-green-800">
-      <SidebarProvider>
-        <Sidebar />
-        <div className="flex-1 min-h-screen transition-all duration-300 bg-orange-50 md:rounded-s-3xl">
-          <div className="p-6">
-            <Route path="/">
-              <Components />
-            </Route>
-            <Route path="/results">
-              <ResultsPage />
-            </Route>
-            <Route path="/calculator">
-              <CalculatorPage />
-            </Route>
-          </div>
-        </div>
-      </SidebarProvider>
-    </main>
-  );
+    return (
+        <main className="relative flex min-h-screen bg-green-800">
+            <SidebarProvider>
+                <Sidebar />
+                <div className="flex-1 min-h-screen transition-all duration-300 bg-orange-50 md:rounded-s-3xl">
+                    <div className="p-6">
+                        <Route path="/">
+                            <Components />
+                        </Route>
+                        <Route path="/results">
+                            <ResultsPage />
+                        </Route>
+                        <Route path="/calculator">
+                            <CalculatorPage />
+                        </Route>
+                    </div>
+                </div>
+            </SidebarProvider>
+        </main>
+    );
 }
 
 export default App;
